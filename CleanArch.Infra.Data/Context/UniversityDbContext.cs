e8c3bf0b11e356d5f75429fabeb4f6edbc3a6159
Fabio Penteado <flzpenteado@gmail.com>
2021-06-10 18:53:06 -0300

    feat: add DbContext

---
+++ CleanArch.Infra.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,15 @@
+using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Course> Courses { get; set; }
+    }
+}
