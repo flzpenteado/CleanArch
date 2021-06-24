b2312485e7b0b96b48a7299b7bc862799c75ce44
Fabio Penteado <flzpenteado@gmail.com>
2021-06-24 18:08:29 -0300

    feat: add authorization to CourseController

--- CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArch.Mvc/Controllers/CourseController.cs
@@ -1,9 +1,11 @@
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Mvc.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private ICourseService _service;
